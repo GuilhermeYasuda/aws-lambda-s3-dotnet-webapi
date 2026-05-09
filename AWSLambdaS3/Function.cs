7da7556766cca884f3d1739d9dbe0f80c9d02402
Guilherme Harunobu Yasuda <haru_yasuda@hotmail.com>
2026-05-09 18:37:02 -0300

    feat: adiciona projeto AWS Lambda S3 em .NET 10.0
    Inclui AWSLambdaS3.csproj, configurações de publicação e execução, handler Function.cs para listar buckets S3, documentação no Readme.md e atualização da solução para incluir o novo projeto.

---
+++ AWSLambdaS3/Function.cs
@@ -0,0 +1,30 @@
+using Amazon.Lambda.Core;
+using Amazon.S3;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace AWSLambdaS3;
+
+public class Function
+{
+
+    ///// <summary>
+    ///// A simple function that takes a string and does a ToUpper
+    ///// </summary>
+    ///// <param name="input">The event for the Lambda function handler to process.</param>
+    ///// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
+    ///// <returns></returns>
+    //public string FunctionHandler(string input, ILambdaContext context)
+    //{
+    //    return input.ToUpper();
+    //}
+
+    public async Task<IEnumerable<string>> FunctionHandler(ILambdaContext context)
+    {
+        var s3Client = new AmazonS3Client();
+        var data = await s3Client.ListBucketsAsync();
+        var buckets = data.Buckets.Select(b => { return b.BucketName; });
+        return buckets;
+    }
+}
